398edf6950ed230d1625d93e6c2b716be8c5951b
greiver179 <te_hung@hotmail.com>
2018-11-13 10:55:13 +0100

    added addUser function, userList and userProfile object in userProfile.js

---
+++ js/userProfile.js
@@ -0,0 +1,29 @@
+
+listOfUsers=[];
+
+userProfile ={
+     name:"Mike",
+     age: 18,
+     userId: 1053,
+     userName: "emperor_c",
+     password: "julius_caesar123",
+     secretQuestion: "who is my most trusted advisor",
+     secretAnswer: "brutus",
+     mail:"truerulerofrome@rome.it"
+};
+
+listOfUsers.push(userProfile)
+
+
+function addUser(name,age,userId,userName,password,secretQuestion,secretAnswer,mail){
+userProfile.name = name;
+userProfile.age = age;
+userProfile.userId = userId;
+userProfile.userName = userName;
+userProfile.password = password;
+userProfile.secretQuestion = secretQuestion;
+userProfile.secretAnswer = secretAnswer;
+userProfile.mail = mail;
+listOfUsers.push(userProfile)
+}
+
